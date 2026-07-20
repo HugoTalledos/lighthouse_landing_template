582f78e933d0a277fac7e5468f135282a6348961
Hugo Talledos <hugo.talledos05@gmail.com>
2026-07-20 11:40:41 -0500

    feat(theme.types): Introduce RawTheme interface for theme configuration options

---
+++ model/theme.types.ts
@@ -0,0 +1,11 @@
+
+export interface RawTheme {
+    primary_color: string;
+    secondary_color: string;
+    font_family: string;
+    bg_color?: string | null;
+    text_color?: string | null;
+    logo_url?: string | null;
+    logo_text?: string | null;
+    logo_icon?: string | null;
+  }
